35675a426685579d27e389f459c21c85edc299f8
Marcus Fredstam <marcus.fredstam@crowdcollective.com>
2024-12-03 22:25:41 +0100

    day 3 part 2

--- day3.py
+++ day3.py
@@ -1,4 +1,5 @@
 import re
+from pprint import pprint
 
 TEST = False
 
@@ -13,7 +14,24 @@ def get_input():
     return lines
 
 re_mul = re.compile("mul\((?P<a>\d{1,3})\,(?P<b>\d{1,3})\)")
+re_enable = re.compile("do\(\)")
+re_disable = re.compile("don\'t\(\)")
 
+def calc_enabled_ranges(enable_pos, disable_pos):
+    enabled_ranges = []
+    for e_pos in enable_pos:
+        prev_d_pos = None
+        for d_pos in reversed(disable_pos):
+            if e_pos > d_pos or (e_pos == enable_pos[-1] and d_pos == disable_pos[-1]):
+                if prev_d_pos:
+                    enabled_ranges.append(range(e_pos, prev_d_pos))
+                else:
+                    enabled_ranges.append(range(e_pos, d_pos))
+                break
+            prev_d_pos = d_pos
+    enabled_ranges.insert(0, range(0, disable_pos[0]))  # Enabled by default
+    pprint(enabled_ranges)
+    return enabled_ranges
 
 def main():
     lines = get_input()
@@ -30,6 +48,25 @@ def main():
 
     # ---PART 2 ---------------------
     part2_solution = 0
+
+    enable_pos = [0]
+    disable_pos = []
+
+    for line in lines:
+        for enable_match in re_enable.finditer(line):
+            enable_pos.append(enable_match.end())
+        for disable_match in re_disable.finditer(line):
+            disable_pos.append(disable_match.end())
+
+    enabled_ranges = calc_enabled_ranges(enable_pos, disable_pos)
+
+    for line in lines:
+        for match in re_mul.finditer(line):
+            for enabled_range in enabled_ranges:
+                if match.start() in enabled_range:
+                    #print("Enabled pos: ", match.group(0))
+                    part2_solution += int(match.group(1)) * int(match.group(2))
+                    break
 
     print("Part 2: ", part2_solution)
 
