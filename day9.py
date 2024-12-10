f7dc5f603e5270c52d0f8e5ae1d98a19284eb00d
Marcus Fredstam <marcus.fredstam@crowdcollective.com>
2024-12-10 22:15:34 +0100

    aoc2024 day 9 part 1

--- day9.py
+++ day9.py
@@ -1,4 +1,5 @@
-TEST = True
+TEST = False
+
 
 def get_input():
     input_file = "input/day9.txt"
@@ -10,76 +11,50 @@ def get_input():
     return lines
 
 
-def get_id_format(disk_map: str) -> str:
-    id_format = ""
-    file_size = True
-    for id, c in enumerate(disk_map):
-        if file_size:
-            id_format = id_format + (str(id//2) * int(c))
-            file_size = False
-        else:
-            id_format = id_format + ("." * int(c))
-            file_size = True
-    return id_format
+def parse(disk_map: str) -> list:
+    disk = []
+    id_value = 0
+    for i, c in enumerate(disk_map):
+        if i % 2 == 0:  # File size
+            for _ in range(0, int(c)):
+                disk.append((id_value,))
+            id_value += 1
+        else:  # Free space
+            for _ in range(0, int(c)):
+                disk.append((".",))
+    return disk
 
 
-def defrag(id_format: str) -> str:
-
-    tester = [
-        # "00...111...2...333.44.5555.6666.777.888899",
-        "009..111...2...333.44.5555.6666.777.88889.",
-        "0099.111...2...333.44.5555.6666.777.8888..",
-        "00998111...2...333.44.5555.6666.777.888...",
-        "009981118..2...333.44.5555.6666.777.88....",
-        "0099811188.2...333.44.5555.6666.777.8.....",
-        "009981118882...333.44.5555.6666.777.......",
-        "0099811188827..333.44.5555.6666.77........",
-        "00998111888277.333.44.5555.6666.7.........",
-        "009981118882777333.44.5555.6666...........",
-        "009981118882777333644.5555.666............",
-        "00998111888277733364465555.66.............",
-        "0099811188827773336446555566..............",
-    ]
-
-    x = 0
-    for idx, c in enumerate(reversed(id_format)):
-        if c == ".":
-            x += 1
+def defrag_disk(disk: list) -> list:
+    for i, x in enumerate(reversed(disk), start=1):
+        if x[0] == ".":
             continue
-        x += 1
-        free_idx = id_format.index(".")
 
-        if free_idx >= (len(id_format) - idx):
+        left_most_free_idx = disk.index((".",))
+
+        if left_most_free_idx > (len(disk) - i):
             break
 
-        id_format = id_format[:free_idx] + c + id_format[free_idx+1:-x] + "."*x
+        disk[left_most_free_idx], disk[len(disk)-i] = disk[len(disk)-i], disk[left_most_free_idx]
+
+    return disk
 
 
-        # print(idx, ":  ", id_format, sep="")
-
-        # if id_format != tester[x-1]:
-        #     print("### CUR: ", id_format, "\n", "### COR: ", tester[x], sep="")
-        #     raise ValueError
-
-    return id_format
-
-
-def calc_checksum(disk: str) -> int:
+def calc_checksum(disk: list) -> int:
     sum = 0
     for i, c in enumerate(disk):
-        if c == ".":
+        if c[0] == ".":
             break
-        sum += (i * int(c))
+        sum += (i * c[0])
     return sum
 
+
 def calculate_checksum(disk_map: str) -> int:
-    id_format = get_id_format(disk_map)
-    # print(id_format)
-    defragged_disk = defrag(id_format)
-    print(defragged_disk)
-    # assert defragged_disk == "0099811188827773336446555566.............."
-    checksum = calc_checksum(defragged_disk)
+    disk = parse(disk_map)
+    disk = defrag_disk(disk)
+    checksum = calc_checksum(disk)
     return checksum
+
 
 def main():
     disk_map = get_input()[0].strip()
@@ -88,8 +63,6 @@ def main():
     part1_solution = 0
 
     part1_solution = calculate_checksum(disk_map)
-
-    low = 90428939430
 
     print("Part 1: ", part1_solution)
 
